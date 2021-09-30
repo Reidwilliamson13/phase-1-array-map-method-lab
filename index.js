4c475b08fb0e10b7b627cc279120307735598c9d
ReidWilliamson <reidwilliamson2@gmail.com>
2021-09-30 17:53:10 -0400

    commit tree 7f3afb6ae9a440a9d36837733bb572b6714a666c

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
 const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
+  "what does the this keyword mean?",
+  "What is the Constructor OO pattern?",
+  "implementing Blockchain Web API",
+  "The Test Driven Development Workflow",
+  "What is NaN and how Can we Check for it",
+  "What is the difference between stopPropagation and preventDefault?",
+  "Immutable State and Pure Functions",
+  "what is the difference between == and ===?",
+  "what is the difference between event capturing and bubbling?",
+  "what is JSONP?",
 ];
 
-const titleCased = () => {
-  return tutorials
-}
+const titleCased = (first) => {
+  // first[0].toUpperCase();
+  // const newTutorial1 = tutorials.split(0, 6, 11, 15, 20, 28));
+  let cap = tutorials.map((first) => {
+    first.split(" ");
+    console.log(first.split(" "));
+    return first
+      .split(" ")
+      .map((word) => word[0].toUpperCase() + word.substring(1))
+      .join(" ");
+  });
+  return cap;
+  // console.log("cap is", cap);
+  // let secondTut = cap.map((second) => console.log(second));
+  // console.log(secondTut);
+  // // secondTut.charAt(0);
+  // cap.map((first) => first[0].toUpperCase());
+  // console.log();
+  // cap.join(" ");
+  // return tutorials;
+};
